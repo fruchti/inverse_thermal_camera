be01d51eff7bef865fdd9101c7b9f6693fbedb86
fruchti <fruchti@gvfr.de>
2018-08-04 17:00:41 +0200

    Add debug LED

--- src/main.c
+++ src/main.c
@@ -5,6 +5,12 @@ int main(void)
     LTP1245_Init();
 
     // LTP1245_FeedPaper(10);
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    GPIOC->CRH = (GPIOC->CRH
+        & ~(0x0f << (4 * PIN_LED - 32)))
+        | (0x01 << (4 * PIN_LED - 32))      // Output, max. 10 MHz
+        ;
 
     char buff[30];
     itoa(BUILD_NUMBER, buff, 10);
